b6032d1911c739375bcd3b826042d9f63212773f
Semyon Fedorov <semund.sigurdsson@gmail.com>
2022-06-02 16:37:08 +0300

    Finished a metro stations escalator repair statistic

---
+++ homework-week-4/2_metro.py
@@ -0,0 +1,21 @@
+import json
+from pprint import pprint
+
+FILE_PATH = "2_metro.json"
+
+
+def get_metro_repair_stations(data_json):
+    with open(data_json, 'r', encoding='cp1251') as json_file:
+        metro_statistic = json.load(json_file)
+    output_stations = set()
+    index = 1
+    for station in metro_statistic:
+        if station['RepairOfEscalators'] and station['NameOfStation'] not in output_stations:
+            print(f"{index}. Станция {station['NameOfStation']}: "
+                  f"Даты выполнения работ - {station['RepairOfEscalators'][0]['RepairOfEscalators']}")
+            index += 1
+            output_stations.add(station['NameOfStation'])
+
+
+if __name__ == '__main__':
+    get_metro_repair_stations(FILE_PATH)
